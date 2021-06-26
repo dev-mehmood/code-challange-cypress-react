062cf0aa43154b0d85706493ca89ced4a37c7653
romesa-dev <dev.romesa@gmail.com>
2021-06-26 19:57:44 +0300

    Solved challange

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,6 +1,10 @@
 describe('Sign Up', () => {
-  it('Adds person to course', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+  it('Adds person to course', () => {
+    // cy.visit('/')
+    cy.wait(1200) // loadPeople apit to fetch data
 
     cy.get('input[name="name"]')
       .click()
@@ -13,7 +17,20 @@ describe('Sign Up', () => {
 
     cy.get('select[name="department"]')
       .select('core')
-      .should('have.value', 'core')
+      .should('have.value', 'core');
+     /* 
+     function apiClient(department) {
+        return {
+          then: function(cb) {
+            setTimeout(() => {
+              cb(Courses[department]);
+            }, 1000);
+          }
+        };
+      }
+     
+     */ 
+    cy.wait(1200) // another 200 ms for render 
 
     cy.get('select[name="course"]')
       .select('git-it')
@@ -22,7 +39,42 @@ describe('Sign Up', () => {
     cy.get('input[type="submit"]')
       .click()
 
+    //   savePeople: function(people) {
+    //     return new Promise((resolve, reject) => {
+    //       setTimeout(() => {
+    //         localStorage.people = JSON.stringify(people);
+    //         return resolve({success: true});
+    //       }, between(3500, 4500));
+    //     });
+    //   }
+    // };
+    
+    // function between (min, max) {
+    //   return Math.random() * (max - min) + min
+    // }
+    cy.wait(4700) // save people api 
+
     cy.get('li')
       .should('contain', 'Some Name - some@email.com - core - git-it')
   })
 })
+
+
+/* 
+   I could be using  the aproach below if it were a network call
+
+    cy.intercept('/activities/*', { fixture: 'activities' }).as('getActivities')
+    cy.intercept('/messages/*', { fixture: 'messages' }).as('getMessages')
+
+    // visit the dashboard, which should make requests that match
+    // the two routes above
+    cy.visit('http://localhost:8888/dashboard')
+
+    // pass an array of Route Aliases that forces Cypress to wait
+    // until it sees a response for each request that matches
+    // each of these aliases
+    cy.wait(['@getActivities', '@getMessages'])
+
+    // these commands will not run until the wait command resolves above
+    cy.get('h1').should('contain', 'Dashboard')
+*/
